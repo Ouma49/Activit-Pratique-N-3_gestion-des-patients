6a6a9a289f401693409547509515723ee44c9add
Oumayma <oumaymadaoudi1949@gmail.com>
2025-03-24 10:21:03 +0000

    part 1 modification

--- src/main/java/ma/enset/hopital/web/PatientController.java
+++ src/main/java/ma/enset/hopital/web/PatientController.java
@@ -1,35 +1,56 @@
 package ma.enset.hopital.web;
 
+import lombok.AllArgsConstructor;
 import ma.enset.hopital.entities.Patient;
 import ma.enset.hopital.repository.PatientRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class PatientController {
-    private final PatientRepository patientRepository;
 
-    @GetMapping("/")
-    public String home() {
-        return "redirect:/index";
-    }
+    private PatientRepository patientRepository;
 
     @GetMapping("/index")
-    public String index(Model model,
-                        @RequestParam(name = "page", defaultValue = "0") int page,
-                        @RequestParam(name = "size", defaultValue = "5") int size,
-                        @RequestParam(name = "keyword", defaultValue = "") String keyword) {
-        Page<Patient> pagePatients = patientRepository.findByNomContainsIgnoreCase(keyword, PageRequest.of(page, size));
-        model.addAttribute("listPatients", pagePatients.getContent());
-        model.addAttribute("pages", new int[pagePatients.getTotalPages()]);
-        model.addAttribute("currentPage", page);
-        model.addAttribute("totalPages", pagePatients.getTotalPages());
-        model.addAttribute("keyword", keyword);
+
+    public String index(Model model) {
+        List<Patient> patients = patientRepository.findAll();
+        model.addAttribute("ListsPatients", patients);
         return "patients";
     }
 }
+    /*public String index(Model model,
+                        @RequestParam(name="page",defaultValue = "0")
+                        int page ,
+                        @RequestParam(name="size",defaultValue = "4")
+                        int size,
+                        @RequestParam(name="keyword",defaultValue = "") String kw){
+        //Pagination
+        Page<Patient> pagePatients = patientRepository.findByNomContains(kw, PageRequest.of(page, size));
+        model.addAttribute("ListsPatients", pagePatients.getContent());
+        model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
+        model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",kw);
+        return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam(name="id") Long id,
+                         @RequestParam(name="keyword",defaultValue = "")String keyword,
+                         @RequestParam(name="page",defaultValue = "0") int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+}*/
